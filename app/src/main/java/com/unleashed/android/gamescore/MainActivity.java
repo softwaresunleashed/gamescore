8a3cb43224a90a0ac4381949c72586658b11ae83
softwaresunleashed <softwares.unleashed@gmail.com>
2017-01-16 20:15:19 +0530

    Minimum UI code fix

--- app/src/main/java/com/unleashed/android/gamescore/MainActivity.java
+++ app/src/main/java/com/unleashed/android/gamescore/MainActivity.java
@@ -1,13 +1,46 @@
 package com.unleashed.android.gamescore;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    LinearLayout scorecard_player1;
+    LinearLayout scorecard_player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initHandle();
+    }
+
+    private void initHandle() {
+        scorecard_player1 = (LinearLayout) findViewById(R.id.player1);
+        scorecard_player1.findViewById(R.id.tv_add).setOnClickListener(this);
+        scorecard_player1.findViewById(R.id.tv_sub).setOnClickListener(this);
+
+        scorecard_player2 = (LinearLayout) findViewById(R.id.player2);
+        scorecard_player2.findViewById(R.id.tv_add).setOnClickListener(this);
+        scorecard_player2.findViewById(R.id.tv_sub).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.tv_add:
+                break;
+
+            case R.id.tv_sub:
+
+                break;
+        }
+
     }
 }
